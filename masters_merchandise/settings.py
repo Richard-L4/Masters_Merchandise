a2b0cec53b10dd4a3338f86dada001b926ab8e5b
Richard-L4 <rwells945@gmail.com>
2025-06-08 12:40:54 +0100

    Stop tracking venv folder

--- masters_merchandise/settings.py
+++ masters_merchandise/settings.py
@@ -11,6 +11,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = False
 
+
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
@@ -25,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -85,20 +88,35 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'masters_merchandise.wsgi.application'
 
-os.environ.setdefault(
-    "DATABASE_URL",
-    "postgresql://neondb_owner:npg_IidRAmuq8V1w@ep-plain-wildflower-a28u503p.eu-central-1.aws.neon.tech/spur_hash_wind_909163"
-)
-
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+    {
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
+    },
+    {
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator'
+        ),
+    },
+    {
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'CommonPasswordValidator'
+        ),
+    },
+    {
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator'
+        ),
+    },
 ]
 
 LANGUAGE_CODE = 'en-us'
@@ -118,3 +136,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
